78da027a74742c02db0bf45434eab8423cbe10be
Clement22400 <Clementveillet@gmail.com>
2018-08-07 13:38:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client()
 var prefix = '!'
 const speak = new Set();
 const kickspeak = new Set();
-bot.login(process.env.TOKEN)
+bot.login("NDY3MjQ2OTI1MDg4MDMwNzIw.DksVcw.EtkMeZuK_jQ-GeFbDIW60WMqL4Y")
 bot.on('ready', function() {
 console.log('Je suis prêt ! #SunLight')
 bot.user.setActivity('!help | SunLight')
